4b02996c433b21c6832703df175ff86f52c1aafa
Camilo Hernandez <jchernandezlo@cesde.net>
2024-10-09 22:01:22 -0500

    segundo cambio

--- app/src/main/java/com/example/productos/MainActivity.java
+++ app/src/main/java/com/example/productos/MainActivity.java
@@ -22,11 +22,11 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
 
         productList = new ArrayList<>();
-        productList.add(new Product("Camiseta", "$15.00", R.drawable.product_image1));
-        productList.add(new Product("Pantalones", "$25.00", R.drawable.product_image2));
-        productList.add(new Product("Chaqueta", "$40.00", R.drawable.product_image3));
-        productList.add(new Product("Zapatos", "$30.00", R.drawable.product_image4));
-        productList.add(new Product("Sombrero", "$10.00", R.drawable.product_image5));
+        productList.add(new Product("Camiseta", "$15.00", R.drawable.product_camiseta));
+        productList.add(new Product("Pantalones", "$25.00", R.drawable.product_pantalon));
+        productList.add(new Product("Chaqueta", "$40.00", R.drawable.product_chaqueta));
+        productList.add(new Product("Zapatos", "$30.00", R.drawable.product_zapatos));
+        productList.add(new Product("Sombrero", "$10.00", R.drawable.product_sombrero));
 
         productAdapter = new ProductAdapter(this, productList);
         recyclerView.setAdapter(productAdapter);
